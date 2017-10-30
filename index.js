cf68a71b1e319d9730e72c1f29d6b1c02dce46b6
Marijn Haverbeke <marijn@haverbeke.nl>
2017-10-30 16:57:33 +0100

    Enable modifier-name kludge on OS X Chrome
    Since it seems to be re-broken there since Chrome 62.
    
    Issue prosemirror/prosemirror#715

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ var chrome = typeof navigator != "undefined" && /Chrome\/(\d+)/.exec(navigator.u
 var safari = typeof navigator != "undefined" && /Apple Computer/.test(navigator.vendor)
 var gecko = typeof navigator != "undefined" && /Gecko\/\d+/.test(navigator.userAgent)
 var mac = typeof navigator != "undefined" && /Mac/.test(navigator.platform)
-var brokenModifierNames = chrome && +chrome[1] < 57 || gecko && mac
+var brokenModifierNames = chrome && (mac || +chrome[1] < 57) || gecko && mac
 
 // Fill in the digit keys
 for (var i = 0; i < 10; i++) base[48 + i] = base[96 + i] = String(i)
